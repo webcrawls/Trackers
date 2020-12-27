43c5e4a93c7bcb77cc5a78e7f4f36c81d9afc432
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-27 15:09:03 -0500

    add PluginModule

---
+++ src/main/java/dev/kscott/itemtrak/inject/PluginModule.java
@@ -0,0 +1,35 @@
+package dev.kscott.itemtrak.inject;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import dev.kscott.itemtrak.ItemTrakPlugin;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Provides JavaPlugin and ItemTrakPlugin.
+ */
+public final class PluginModule extends AbstractModule {
+
+    /**
+     * ItemTrakPlugin reference.
+     */
+    private final @NonNull ItemTrakPlugin plugin;
+
+    /**
+     * Constructs PluginModule.
+     *
+     * @param plugin ItemTrakPlugin reference.
+     */
+    public PluginModule(final @NonNull ItemTrakPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public final void configure() {
+        this.bind(JavaPlugin.class).toInstance(plugin);
+        this.bind(ItemTrakPlugin.class).toInstance(plugin);
+    }
+
+}
