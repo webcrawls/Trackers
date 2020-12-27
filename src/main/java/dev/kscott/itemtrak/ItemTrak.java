2c1dc3cd0a8509b3171382eaeda7e89ed9145d62
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-27 15:02:47 -0500

    add base project

---
+++ src/main/java/dev/kscott/itemtrak/ItemTrak.java
@@ -0,0 +1,17 @@
+package dev.kscott.itemtrak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemTrak extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
