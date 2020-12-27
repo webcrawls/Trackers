1df04a7c1141019d4c96438f1d28947052f77133
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-27 15:10:53 -0500

    Add injector

--- src/main/java/dev/kscott/itemtrak/ItemTrakPlugin.java
+++ src/main/java/dev/kscott/itemtrak/ItemTrakPlugin.java
@@ -1,17 +1,24 @@
 package dev.kscott.itemtrak;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import dev.kscott.itemtrak.inject.PluginModule;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
+/**
+ * The ItemTrak base plugin class.
+ */
 public final class ItemTrakPlugin extends JavaPlugin {
 
+    /**
+     * Registers important plugin stuff
+     */
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        final @NonNull Injector injector = Guice.createInjector(
+                new PluginModule(this)
+        );
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
