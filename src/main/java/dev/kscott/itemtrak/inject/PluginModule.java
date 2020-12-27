1e7cc424ed5f9df84999aa35297ca529694fd7c3
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-27 15:32:34 -0500

    provide BukkitAudiences in PluginModule

--- src/main/java/dev/kscott/itemtrak/inject/PluginModule.java
+++ src/main/java/dev/kscott/itemtrak/inject/PluginModule.java
@@ -2,6 +2,8 @@ package dev.kscott.itemtrak.inject;
 
 import com.google.inject.AbstractModule;
 import dev.kscott.itemtrak.ItemTrakPlugin;
+import net.kyori.adventure.platform.AudienceProvider;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -16,21 +18,30 @@ public final class PluginModule extends AbstractModule {
     private final @NonNull ItemTrakPlugin plugin;
 
     /**
+     * BukkitAudiences reference.
+     */
+    private final @NonNull BukkitAudiences audiences;
+
+    /**
      * Constructs PluginModule.
      *
      * @param plugin ItemTrakPlugin reference.
      */
     public PluginModule(final @NonNull ItemTrakPlugin plugin) {
         this.plugin = plugin;
+        this.audiences = BukkitAudiences.create(plugin);
     }
 
     /**
-     * Configures Guice to provide JavaPlugin and ItemTrakPlugin
+     * Configures Guice to provide plugin and audience stuff
      */
     @Override
     public final void configure() {
         this.bind(JavaPlugin.class).toInstance(plugin);
         this.bind(ItemTrakPlugin.class).toInstance(plugin);
+
+        this.bind(AudienceProvider.class).toInstance(audiences);
+        this.bind(BukkitAudiences.class).toInstance(audiences);
     }
 
 }
