90165d066c3f8a94ed2bb8b4518b3301eb5c9fdc
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-28 02:15:16 -0500

    register TrackerModule

--- src/main/java/dev/kscott/itemtrak/ItemTrakPlugin.java
+++ src/main/java/dev/kscott/itemtrak/ItemTrakPlugin.java
@@ -4,6 +4,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import dev.kscott.itemtrak.inject.CommandModule;
 import dev.kscott.itemtrak.inject.PluginModule;
+import dev.kscott.itemtrak.inject.TrackerModule;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -19,7 +20,8 @@ public final class ItemTrakPlugin extends JavaPlugin {
     public void onEnable() {
         final @NonNull Injector injector = Guice.createInjector(
                 new PluginModule(this),
-                new CommandModule(this)
+                new CommandModule(this),
+                new TrackerModule()
         );
 
 
