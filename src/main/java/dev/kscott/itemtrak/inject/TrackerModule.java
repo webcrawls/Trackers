96bf014d0da7591eb2df8c5ad694e870b510317b
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-28 02:46:11 -0500

    remove unused imports

--- src/main/java/dev/kscott/itemtrak/inject/TrackerModule.java
+++ src/main/java/dev/kscott/itemtrak/inject/TrackerModule.java
@@ -1,7 +1,6 @@
 package dev.kscott.itemtrak.inject;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
@@ -10,13 +9,6 @@ import dev.kscott.itemtrak.tracker.TrackerManager;
 import dev.kscott.itemtrak.tracker.TrackerRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
-
-import javax.inject.Qualifier;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 public class TrackerModule extends AbstractModule {
 
@@ -32,7 +24,7 @@ public class TrackerModule extends AbstractModule {
     public @NonNull TrackerManager providesTrackerManager(
             @Named("TrackerRegistry") final @NonNull TrackerRegistry registry,
             @Named("JavaPlugin") final @NonNull JavaPlugin plugin
-            ) {
+    ) {
         return new TrackerManager(plugin, registry);
     }
 
