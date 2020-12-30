e8bf48258ed73aa5ba062b802338d20212c68111
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-30 08:44:06 -0500

    Add TrackerData

---
+++ src/main/java/dev/kscott/itemtrak/tracker/TrackerData.java
@@ -0,0 +1,31 @@
+package dev.kscott.itemtrak.tracker;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class TrackerData {
+
+    private final @NonNull TrackerConfig trackerConfig;
+
+    private int count;
+
+    public TrackerData(final @NonNull TrackerConfig trackerConfig, final int count) {
+        this.trackerConfig = trackerConfig;
+        this.count = count;
+    }
+
+    public void incrementCount() {
+        count++;
+    }
+
+    public void incrementCount(final int amount) {
+        count += amount;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public @NonNull TrackerConfig getTrackerConfig() {
+        return trackerConfig;
+    }
+}
